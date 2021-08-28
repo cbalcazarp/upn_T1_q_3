30c2a33d94272bea3b3b592d0b2d4e6f942ce564
Carlos Balcazar <cbalcazarp@gmail.com>
2021-08-28 10:52:13 -0500

    code added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace upn_T1_q_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Double lado1, lado2, areaRectangulo;
+            Console.WriteLine("Bienvenido al programa de cálculo de perímetro de un rectangulo");
+            Console.Write("Ingrese lado1: ");
+            lado1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese lado2: ");
+            lado2 = Convert.ToDouble(Console.ReadLine());
+            areaRectangulo = 2 * (lado1 + lado2);
+            Console.WriteLine("El perímetro del rectangulo es: " + Convert.ToString(areaRectangulo));
+
         }
     }
 }
